9cc3b361275bea8c7942de9fa76862106e30d617
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-08-23 16:03:20 +0100

    fix errors

--- models/message.js
+++ models/message.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const MessageSchema = ({
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     title: { type: String, minLength: 1, maxLength: 30, required: true },
-    title: { type: String, minLength: 1, maxLength: 250, required: true },
+    text: { type: String, minLength: 1, maxLength: 250, required: true },
     date: { type: Date, default: Date.now, required: true }
 });
 
-module.exports = mongoose.module("Message", MessageSchema)
+module.exports = mongoose.model("Message", MessageSchema)
