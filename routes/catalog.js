a31959ab00267ae21e974884f32baf40bf831e63
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-08-24 16:48:33 +0100

    added sign-up post

--- routes/catalog.js
+++ routes/catalog.js
@@ -9,4 +9,7 @@ router.get('/', user_controller.home_get)
 //GET user Sign-Up form
 router.get('/sign-up', user_controller.sign_up_get)
 
+//POST user Sign-Up form
+router.post('/sign-up', user_controller.sign_up_post)
+
 module.exports = router;
