06ce69afee883e0fd20358fea27fcf2fee3884f3
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-08-24 16:19:09 +0100

    added catalog for routes

---
+++ routes/catalog.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const user_controller = require('../controllers/userController');
+
+//GET Home Page
+router.get('/', user_controller.home_get)
+
+//GET user Sign-Up form
+router.get('/sign-up', user_controller.sign_up_get)
+
+module.exports = router;
