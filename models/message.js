a3ba9183677e50cc0f27b573e991cc42fae58f65
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-08-25 16:02:03 +0100

    refactor MessageSchema

--- models/message.js
+++ models/message.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = ({
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    userID: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    username: { type: String, required: true },
     title: { type: String, minLength: 1, maxLength: 30, required: true },
     text: { type: String, minLength: 1, maxLength: 250, required: true },
     date: { type: Date, default: Date.now, required: true }
